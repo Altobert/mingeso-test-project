1dbfa13cabf1aa4b004cacda22c87ab481c5d70a
Altobert <alberto.sanmartin@usach.cl>
2023-09-29 23:37:45 -0300

    primer commit ptojecto ejemplo

---
+++ src/main/java/cl/msdocker/app/AppMsDockerApplication.java
@@ -0,0 +1,13 @@
+package cl.msdocker.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMsDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/msdocker/app/AppMsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.msdocker.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
